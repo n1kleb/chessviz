b5a02f278d4f234bae58c4e10158043db9ca0d6d
n1kleb <belkin.2001@mail.ru>
2020-09-29 00:08:55 +0700

    Function declarations and type definitions for board.c

---
+++ src/board.h
@@ -0,0 +1,11 @@
+#ifndef BOARD
+#define BOARD
+#define m 10
+
+void str_scan(char* ans);
+void init_board(char (*board)[m]);
+int step_white(char (*board)[m], int ffc, int fsc, int sfc, int ssc);
+int step_black(char (*board)[m], int ffc, int fsc, int sfc, int ssc);
+int check_coordinats(int* ffc, int* fsc, int* sfc, int* ssc);
+
+#endif
