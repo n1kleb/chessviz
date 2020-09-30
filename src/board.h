b7ec9b1128dd7c08667f816a7fc990a83de3ecb3
n1kleb <belkin.2001@mail.ru>
2020-10-01 01:20:17 +0700

    Add void check_kings

--- src/board.h
+++ src/board.h
@@ -4,6 +4,7 @@
 
 void str_scan(char* ans);
 void init_board(char (*board)[m]);
+void check_kings(char (*board)[m]);
 int step_white(char (*board)[m], int ffc, int fsc, int sfc, int ssc);
 int step_black(char (*board)[m], int ffc, int fsc, int sfc, int ssc);
 int check_coordinats(int* ffc, int* fsc, int* sfc, int* ssc);
